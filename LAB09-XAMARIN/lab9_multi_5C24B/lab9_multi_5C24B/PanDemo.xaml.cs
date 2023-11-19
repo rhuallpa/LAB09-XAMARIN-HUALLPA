7544baf35bd3ba4891659519892812a2ca332c2f
rhuallpa <rebeca.huallpa@tecsup.edu.pe>
2023-11-18 20:18:43 -0500

    first commit

---
+++ LAB09-XAMARIN/lab9_multi_5C24B/lab9_multi_5C24B/PanDemo.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab9_multi_5C24B
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PanDemo : ContentPage
+    {
+        public PanDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LAB09-XAMARIN/lab9_multi_5C24B/lab9_multi_5C24B/PinchDemo.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab9_multi_5C24B
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PinchDemo : ContentPage
+    {
+        public PinchDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
